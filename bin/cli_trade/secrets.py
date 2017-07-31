2438024d2b8aaaa71074341703088008624d6c90
Andre Banks <andrebanks.ab@gmail.com>
2017-07-31 15:32:48 -0400

    setup

---
+++ bin/cli_trade/secrets.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+'''
+Store Bittrex API key and secret
+'''
+
+
+BKEY = 'd820a092f8cd4a46a11a29fa7aeedb76'
+BSECRET = '01ddcff49bd24473a69d4b756682db72'
+
+
--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
-from distutils.core import setup
+"""
+Inital project Setup
+"""
+
+
+from distutils.core import setup, find_packages
 
 setup(
     name='BittrexCliTrader',
     version='1.0',
     author='Andre V. Banks',
     author_email='andrebanks.ab@gmail.com',
-    description=("A command line application that allows for trading of coins
+    description=("A command line application that allows for trading of coins\
                  found on Bittrex.com using the Bittrex.com API"),
     license='MIT',
     keywords="bittrex cryptocurrency coinmarketcap cryptotrading",
     url="https://github.com/avbanks/Bittrex-CommandLine-Trader",
-    packages=['Bittrex'],
+    packages=find_packages(),
     long_description=read('README'),
 )
 
